77dd81b09cd3859e41dced8af43642f4d8efea11
asahi417 <asahi1992ushio@gmail.com>
2024-10-07 00:45:34 +0900

    init

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 with open('README.md', 'r', encoding="utf-8") as f:
     readme = f.read()
 
-version = '0.0.0'
+version = '0.0.1'
 setup(
     name='tts_eval',
     packages=find_packages(exclude=["tests", "scripts"]),
