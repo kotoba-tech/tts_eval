178cf2c0f601d8d54bb91ff7b68ee8eddb901527
asahi417 <asahi1992ushio@gmail.com>
2024-10-28 23:03:46 +0900

    init

--- experiments/test_tts_output/create_evaluation_dataset.py
+++ experiments/test_tts_output/create_evaluation_dataset.py
@@ -2,20 +2,26 @@ from random import seed, shuffle
 from itertools import chain
 from datasets import load_dataset, concatenate_datasets, DatasetDict
 
-subsets = [
-    "CVtY0yrLnyPqbcJb0NF8c6w",
-    "CUOdWLM-qyApaHvW8SyviHg",
-    "ChrisSasaki2020",
-    "DISCOVER_WASEDA",
-    "JINNAI-NETAJIN"
+repo_id = "kotoba-speech/tts_evaluation_v2"
+source_repo_id = "kotoba-speech/youtube_crawl_audio_tscribed_audio_denoised_44_1kHz"
+subsets_in = [
+    ["voice_actors_academia", "jQ3MQyGqBBI"],
+    ["joiito", "htAzZqnmlas"],
+    ["weathernewstvch", "Ooeu7HKKBb8"],
+    ["AppleTaka", "T_oVAW2L"],
+    ["GenRonTV", "ZNyAcZsHBYY"],
+    ["yuni_tarot", "V4O62I-8Sss"]
 ]
-columns = [
-    "key", "dataset_id", "audio", "text", "duration", "snr", "speaking_rate"
+subsets_out = [
+    ["nlp2980", "rxkXohKUthg"],
+    ["citybunkyotv", "dRPIEJrtDS4"]
 ]
+columns = ["key", "dataset_id", "audio", "text", "duration", "snr", "speaking_rate"]
 
 
-def get_dataset(subset: str, sample_size: int = 100, n_additional_transcripts_per_sample: int = 4):
-    dataset = load_dataset("kotoba-speech/youtube_crawl_audio_tscribed_audio", subset, split="train")
+def get_dataset(subset: str, key: str, sample_size: int = 25, n_additional_transcripts_per_sample: int = 4):
+    dataset = load_dataset(source_repo_id, subset, split="train")
+    dataset = dataset.filter(lambda x: key in x["key"])
     dataset = dataset.filter(lambda x: x["snr"] > 15)
     dataset = dataset.filter(lambda x: x["speaking_rate"] > 10)
     dataset = dataset.filter(lambda x: x["speaking_rate"] < 50)
@@ -30,25 +36,28 @@ def get_dataset(subset: str, sample_size: int = 100, n_additional_transcripts_pe
     return dataset, text_transcript, text
 
 
-dataset_list = []
-text_transcript = []
-text = []
-for s in subsets:
-    _dataset, _text_transcript, _text = get_dataset(s)
-    dataset_list.append(_dataset)
-    text_transcript.append(_text_transcript)
-    text.append(_text)
-new_dataset = concatenate_datasets(dataset_list)
-new_text_transcript = list(chain(*text_transcript))
-new_dataset = new_dataset.add_column("transcript", new_text_transcript)
-new_dataset = new_dataset.add_column("text", new_text_transcript)
-new_text = list(chain(*text))
-seed(42)
-shuffle(new_text)
-dataset_list = [new_dataset]
-for s in range(0, len(new_text), len(new_text_transcript)):
-    new_dataset = new_dataset.remove_columns("text")
-    new_dataset = new_dataset.add_column("text", new_text[s: s + len(new_text_transcript)])
-    dataset_list.append(new_dataset)
-new_dataset = concatenate_datasets(dataset_list)
-DatasetDict({"test": new_dataset}).push_to_hub("kotoba-speech/tts_evaluation_v1", private=True)
+dataset_dict = {}
+for split_name, subsets in zip(["test_in", "test_out"], [subsets_in, subsets_out]):
+    dataset_list = []
+    text_transcript_list = []
+    text_list = []
+    for s, k in subsets:
+        d, t_t, t = get_dataset(s, k)
+        dataset_list.append(d)
+        text_transcript_list.append(t_t)
+        text_list.append(t)
+    new_dataset = concatenate_datasets(dataset_list)
+    new_text_transcript = list(chain(*text_transcript_list))
+    new_dataset = new_dataset.add_column("transcript", new_text_transcript)
+    new_dataset = new_dataset.add_column("text", new_text_transcript)
+    new_text = list(chain(*text_list))
+    seed(42)
+    shuffle(new_text)
+    dataset_list = [new_dataset]
+    for s in range(0, len(new_text), len(new_text_transcript)):
+        new_dataset = new_dataset.remove_columns("text")
+        new_dataset = new_dataset.add_column("text", new_text[s: s + len(new_text_transcript)])
+        dataset_list.append(new_dataset)
+    new_dataset = concatenate_datasets(dataset_list)
+    dataset_dict[split_name] = new_dataset
+DatasetDict(dataset_dict).push_to_hub(repo_id, private=True)
